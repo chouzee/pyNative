f45af42239a722666aafe6a35c6ed12a0cdb8537
chouzee <anton.ryabchenko.92@gmail.com>
2020-11-26 10:21:04 +0200

    Return sum or multiply in condition

--- q1.py
+++ q1.py
@@ -6,3 +6,18 @@ def multiplication(num1, num2):
         return total
 
 print(multiplication(10, 12))
+
+# FROM SOLUTION SECTION
+def multiplication_or_sum(num1, num2):
+    product = num1 * num2
+    if product <= 1000:
+        return product
+    else:
+        return num1 + num2
+
+number1 = 20
+number2 = 30
+
+print("\n")
+result = multiplication_or_sum(number1, number2)
+print("The result is", result)
