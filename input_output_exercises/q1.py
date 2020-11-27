a2766b8e9ef06a3b0eb9f188904d7581ea41c44e
chouzee <anton.ryabchenko.92@gmail.com>
2020-11-27 09:55:31 +0200

    numbers multiplications

---
+++ input_output_exercises/q1.py
@@ -0,0 +1,9 @@
+num = int(input("Type first number: "))
+num1 = int(input("Type second number: "))
+
+def calculation(number, number1):
+    return number * number1
+
+
+print(calculation(num, num1))
+
