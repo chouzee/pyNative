4e5ade8582de6828e5b97c9a2b1a0678615c159f
chouzee <anton.ryabchenko.92@gmail.com>
2020-11-27 09:00:50 +0200

    Update with section solution

--- q10.py
+++ q10.py
@@ -17,3 +17,21 @@ s_list = [13, 43, 24, 36, 12]
 
 new_list(f_list, s_list)
 
+# FROM SOLUTION SECTION
+def mergeList(listOne, listTwo):
+    print("First List ", listOne)
+    print("Second List ", listTwo)
+    thirdList = []
+    
+    for num in listOne:
+        if (num % 2 != 0):
+            thirdList.append(num)
+    for num in listTwo:
+        if (num % 2 == 0):
+            thirdList.append(num)
+    return thirdList
+
+listOne = [10, 20, 23, 11, 17]
+listTwo = [13, 43, 24, 36, 12]
+
+print("result List is", mergeList(listOne, listTwo))
