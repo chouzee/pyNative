3cbb013f097543f646cd85ebc4650cb9ed4644f5
chouzee <anton.ryabchenko.92@gmail.com>
2020-11-28 09:02:02 +0200

    print digit pattern downward

--- conditions_loops_range/q7.py
+++ conditions_loops_range/q7.py
@@ -1,7 +1,6 @@
 rows = 5
 
-for i in range(rows+1, 0, -1):
-    for j in range(i-1, 0, -1):
+for i in range(0, rows+1):
+    for j in range(rows-i,0,-1):
         print(j, end=" ")
-        
     print()
