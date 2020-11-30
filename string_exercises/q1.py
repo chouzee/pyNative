7c94160d4381c594174a8c61d513383f6f2f85e0
chouzee <anton.ryabchenko.92@gmail.com>
2020-11-30 15:59:19 +0200

    print middle chars

---
+++ string_exercises/q1.py
@@ -0,0 +1,8 @@
+def middle_chars(str1):
+    middle = int(len(str1) / 2)
+    middleThree = str1[middle-1:middle+2]
+    print(middleThree)
+
+str1 = "JhonDipPeta"
+str1 = "JaSonAy"
+middle_chars(str1)
