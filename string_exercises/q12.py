b23b9d2c132bec61aa814e38ce6d950a40d52d51
chouzee <anton.ryabchenko.92@gmail.com>
2020-12-01 10:14:22 +0200

    return the biggerst index

---
+++ string_exercises/q12.py
@@ -0,0 +1,7 @@
+
+
+str1 = "Emma is a data scientist who knows Python. Emma works at google."
+substr = "Emma"
+# rfind() returns the biggest index of word occurence
+res = str1.rfind(substr)
+print(res)
