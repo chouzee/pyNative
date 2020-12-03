6fe580a3548015d260a7dc75e57a858b7aee1ce8
chouzee <anton.ryabchenko.92@gmail.com>
2020-12-03 23:19:30 +0200

    iterate over list and compare value with dict val

--- data_structure_exercise/q8.py
+++ data_structure_exercise/q8.py
@@ -5,3 +5,13 @@ for i in rollNumber:
     if i in sampleDict.values():
         el.append(i)
 print("after removing unwanted elements from list ", el)
+
+# FROM SOLUTION SECTION
+rollNumber  = [47, 64, 69, 37, 76, 83, 95, 97]
+sampleDict  ={'Jhon':47, 'Emma':69, 'Kelly':76, 'Jason':97} 
+
+print("List -", rollNumber)
+print("Dictionary - ", sampleDict)
+
+rollNumber[:] = [item for item in rollNumber if item in sampleDict.values()]
+print("after removing unwanted elemnts from list ", rollNumber)
