1a14ab767600f35f85dd65b552e51c00e35e5cbc
chouzee <anton.ryabchenko.92@gmail.com>
2020-12-03 23:27:35 +0200

    rm duplicates, create tuple, print min, max

---
+++ data_structure_exercise/q10.py
@@ -0,0 +1,5 @@
+sampleList = [87, 45, 41, 65, 94, 41, 99, 94]
+print("Uniques values ", list(set(sampleList)))
+print("tuple ", tuple(set(sampleList)))
+print(min(sampleList))
+print(max(sampleList))
