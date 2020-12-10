b7fc08c41a48f8235a7c5e6437a65a577232dcbc
chouzee <anton.ryabchenko.92@gmail.com>
2020-12-10 11:57:19 +0200

    Add comments to describe methods

--- mysql_python_tutorial/db_connection.py
+++ mysql_python_tutorial/db_connection.py
@@ -2,14 +2,21 @@ import mysql.connector
 from mysql.connector import Error
 
 try:
+    # mysql.connector.connect() allow us to pass db parameters \
+    # using for connection
+    
+    # connect() method establish a connection to db from Python app \
+    # and return a MySQLConnection object 
     connection = mysql.connector.connect(host='localhost',
                                          database='Electronics',
                                          user='pynative',
                                          password='pynative@#29')
     
+    # is_connected() verifies our connection from app to db
     if connection.is_connected():
         db_Info = connection.get_server_info()
         print("Connected to MySQL server version ", db_Info)
+        # using cursor object we can execute SQL queries
         cursor = connection.cursor()
         # cursor.execute()  SQL queries from Python
         cursor.execute("select database();")
