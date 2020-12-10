375373e59cef6cd5b87157968fee3c9fed388d1a
chouzee <anton.ryabchenko.92@gmail.com>
2020-12-10 12:31:45 +0200

    Added timeouts to db connection

--- mysql_python_tutorial/db_connection.py
+++ mysql_python_tutorial/db_connection.py
@@ -2,33 +2,37 @@ import mysql.connector
 from mysql.connector import Error
 
 try:
-    # mysql.connector.connect() allow us to pass db parameters \
-    # using for connection
-    
-    # connect() method establish a connection to db from Python app \
-    # and return a MySQLConnection object 
     connection = mysql.connector.connect(host='localhost',
                                          database='Electronics',
                                          user='pynative',
-                                         password='pynative@#29')
-    
-    # is_connected() verifies our connection from app to db
+                                         password='pynative@#29',
+                                         connection_timeout=180)
+
     if connection.is_connected():
         db_Info = connection.get_server_info()
         print("Connected to MySQL server version ", db_Info)
-        # using cursor object we can execute SQL queries
         cursor = connection.cursor()
-        # cursor.execute()  SQL queries from Python
+
+        # global connection timeout arguments
+        global_connect_timeout = 'SET GLOBAL connect_timeout=180'
+        global_wait_timeout = 'SET GLOBAL connect_timeout=180'
+        global_interactive_timeout = 'SET GLOBAL connect_timeout=180'
+
+        cursor.execute(global_connect_timeout)
+        cursor.execute(global_wait_timeout)
+        cursor.execute(global_interactive_timeout)
+
+        connection.commit()
+
         cursor.execute("select database();")
         record = cursor.fetchone()
         print("You're connected to database: ", record)
-        
+
+
 except Error as e:
     print("Error while connecting to MySQL", e)
 finally:
     if (connection.is_connected()):
-        # close cursor object
         cursor.close()
-        # close db connection
         connection.close()
         print("MySQL connection is closed")
