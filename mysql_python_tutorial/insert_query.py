b134aaafcfc90c687c9969b971b2a90496542259
chouzee <anton.ryabchenko.92@gmail.com>
2020-12-11 09:30:39 +0200

    insert using variables

--- mysql_python_tutorial/insert_query.py
+++ mysql_python_tutorial/insert_query.py
@@ -2,25 +2,32 @@ import mysql.connector
 from mysql.connector import Error
 from mysql.connector import errorcode
 
-try:
-    connection = mysql.connector.connect(host='localhost',
-                                         database='Electronics',
-                                         user='pynative',
-                                         password='pynative@#29')
-    mySql_insert_query = """INSERT INTO Laptop (Id, Name, Price, Purchase_date)
-                            VALUES
-                            (10, 'Lenovo ThinkPad P71', 6459, '2019-08-14')"""
+def insert_vars_into_table(id, name, price, purchase_date):
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                             database='Electronics',
+                                             user='pynative',
+                                             password='pynative@#29')
 
-    cursor = connection.cursor()
-    cursor.execute(mySql_insert_query)
-    connection.commit()
-    print(cursor.rowcount, "Record inserted successfully into Laptop table")
-    cursor.close()
+        cursor = connection.cursor()
+        mySql_insert_query = """INSERT INTO Laptop (Id, Name, Price, Purchase_date)
+                                VALUES
+                                (%s, %s, %s, %s)"""
 
-except mysql.connector.Error as error:
-    print(f"Failed to insert record into Laptop table {error}")
+        recordTuple = (id, name, price, purchase_date)
+        cursor.execute(mySql_insert_query, recordTuple)
+        connection.commit()
+        print(cursor.rowcount, "Record inserted successfully into Laptop table")
 
-finally:
-    if (connection.is_connected()):
-        connection.close()
-        print("MySQL connection is closed")
+    except mysql.connector.Error as error:
+        print(f"Failed to insert record into Laptop table {error}")
+
+    finally:
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+
+insert_vars_into_table(2, 'Area 51M', 6999, '2019-04-14')
+insert_vars_into_table(3, 'MacBook Pro', 2499, '2019-06-20')
